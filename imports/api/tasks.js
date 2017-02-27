6bed468d38ca9f9264159aa5cc46f7dbd7e77a3e
Kotokrad <dsl2407@gmail.com>
2017-02-27 19:53:42 +0200

    Add task scheduling

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,5 +1,28 @@
-import { SyncedCron } from 'percolate:synced-cron';
+import { Meteor } from 'meteor/meteor';
+import { SyncedCron } from 'meteor/percolate:synced-cron';
+import Fiber from 'fibers';
 
-function addNotification() {
+export function scheduleTrackGeneration(satellite) {
+  SyncedCron.add({
+    name: `${satellite}.track`,
+    schedule: parser => parser.recur().every(2).hour(),
+    job() {
+      Fiber(() => {
+        Meteor.call('tracks.cleanup');
+        Meteor.call('tracks.generate', satellite);
+      }).run();
+    },
+  });
+}
 
+export function scheduleTLEUpdate() {
+  SyncedCron.add({
+    name: `TLE update`,
+    schedule: parser => parser.recur().every(1).weekOfYear(),
+    job() {
+      Fiber(() => {
+        Meteor.call('elements.update');
+      }).run();
+    },
+  });
 }
--- server/main.js
+++ server/main.js
@@ -1,11 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import '../imports/api/tracks.js';
 import '../imports/api/elements.js';
+import { scheduleTrackGeneration, scheduleTLEUpdate } from '../imports/api/tasks';
+
+import { Elements } from '../imports/api/elements.js';
+
+const satelliteList = ['noaa-16', 'noaa-19']; // TODO: get list from elements collection:
+// Elements.rawCollection().distinct('satellite').then(result => {
+//   console.log(result);
+// });
 
 Meteor.startup(() => {
   // code to run on server at startup
   // Meteor.call('tracks.clear');
   Meteor.call('elements.update')
   Meteor.call('tracks.cleanup');
-  Meteor.call('tracks.generate', 'noaa-19');
+  scheduleTLEUpdate();
+  satelliteList.forEach(satellite => {
+    Meteor.call('tracks.generate', satellite);
+    scheduleTrackGeneration(satellite)
+  });
+  SyncedCron.start();
 });
